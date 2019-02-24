5ed9e338655c14ffe739f11cddc38c7ae16e4adb
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-02-24 16:23:32 +0100

    added gallery.js

---
+++ script/gallery.js
@@ -0,0 +1,19 @@
+const images = [];
+
+for (let i = 1; i <= 22; i++) {
+  images[i-1] = "../img/gallery/gallery_(" + i + ").jpg";
+}
+
+for (image of images) {
+  console.log(image);
+}
+
+let img = document.getElementById("images");
+let elements = [];
+
+	for (let i = 0; i < images.length; i++) {
+		elements[i] = document.createElement("img");
+		elements[i].src = images[i];
+    console.log("Elements " + i + elements[i]);
+		img.appendChild(elements[i]);
+	}
