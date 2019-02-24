94aca9397cdc52530f13edc9618bf116290d43f5
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-02-24 19:25:54 +0100

    fixed bug

--- script/load.js
+++ script/load.js
@@ -1,3 +1,4 @@
 window.addEventListener("load", function () {
-  let body = document.getElementById('body').style = "block";
+  let body = document.getElementById('body').style.display = "block";
+  let loader = document.getElementById('loader').style.display = "none";
 });
