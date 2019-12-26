620d0ba78dd09752a458c520032d5242b2b23254
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-12-26 15:02:15 +0100

    modify gallery.js

--- script/gallery.js
+++ script/gallery.js
@@ -7,26 +7,41 @@ window.addEventListener('load', () => {
   const imageWrapper = document.getElementById('slideshowImages');
   const nextImage = document.getElementById('nextImage');
   const previousImage = document.getElementById('previousImage');
+  const disableSlideshow = document.getElementById('disableSlideshow');
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBd4ZNQRImSA-DLkRuQUShh8jqH-L9DVJM",
+    authDomain: "jonas-dorfinger.firebaseapp.com",
+    databaseURL: "https://jonas-dorfinger.firebaseio.com",
+    projectId: "jonas-dorfinger",
+    storageBucket: "jonas-dorfinger.appspot.com",
+    messagingSenderId: "47344971189",
+    appId: "1:47344971189:web:7dd58623493ae508"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
   printImages();
 
   closeSlideshow.addEventListener('click', () => {
     slideshowWrapper.style.opacity = 0;
     imageWrapper.style.transform = 'scale(0.6)';
+    disableSlideshow.classList.add('hide');
+    document.body.classList.remove('overflowHidden');
 
     setTimeout(() => {
       slideshowWrapper.classList.add('hide');
     }, 310);
   });
 
-  slideshowWrapper.addEventListener('click', () => {
-    // closeSlideshow.click();
+  disableSlideshow.addEventListener('click', () => {
+    closeSlideshow.click();
   });
 
   nextImage.addEventListener('click', () => {
     currentImage++;
 
-    if (currentImage === 23) {
+    if (currentImage === howManyImages + 1) {
       currentImage = 1;
     }
 
@@ -37,7 +52,7 @@ window.addEventListener('load', () => {
     currentImage--;
 
     if (currentImage === 0) {
-      currentImage = 22;
+      currentImage = howManyImages;
     }
 
     document.getElementById('currentImage').src = `../img/gallery/gallery (${currentImage}).jpg`;
@@ -46,7 +61,7 @@ window.addEventListener('load', () => {
 
 window.addEventListener('resize', () => {
   initButtonWrapper();  
-})
+});
 
 function printImages() {
   const contentSlideshowWrapper = document.querySelector('#images');
@@ -72,6 +87,9 @@ function printImages() {
       document.getElementById('currentImage').src = `../img/gallery/gallery (${i+1}).jpg`;
 
       initButtonWrapper();
+
+      disableSlideshow.classList.remove('hide');
+      document.body.classList.add('overflowHidden');
       
       setTimeout(() => {
         slideshowWrapper.style.opacity = 1;
@@ -83,21 +101,105 @@ function printImages() {
     
     rowCounter++;
     
-    if ((rowCounter) % 3 === 0 || i === 15) {
+    if (((rowCounter) % 3 === 0 || i === 15) && window.innerWidth >= 1160) {
       contentSlideshowWrapper.appendChild(newRow);
       newRow = document.createElement('div');
       rowCounter = 0;
+    } else if (window.innerWidth < 1160) {
+      contentSlideshowWrapper.appendChild(newImage);
     }
   }
 }
 
 function initButtonWrapper() {
-  const buttonWrapper = document.getElementsByClassName('buttonWrapper');
-
-  console.log(document.getElementById('currentImage').clientHeight + 'px');
-  
+  const buttonWrapper = document.getElementsByClassName('buttonWrapper');  
   
   for (const wrapper of buttonWrapper) {
     wrapper.style.height = document.getElementById('currentImage').clientHeight + 'px';
   }
+}
+
+function getPaths() {
+  const storage = firebase.storage();
+  const storageRef = storage.ref();
+  const directory = storageRef.child('gallery');
+  const paths = [];
+
+  directory.listAll().then((res) => {
+    for (const image of res['items']) {
+      const imagesRef = storageRef.child(image.fullPath);
+      
+      imagesRef.getDownloadURL().then((url) => {
+        paths.push(url);
+
+        if (paths.length >= 2) {
+          for (let i = 0; i < paths.length; i++) {
+            for (let j = i; j < paths.length; j++) {
+              console.log(paths);
+              
+              let path1 = paths[i].location.path;
+              let path2 = paths[j].location.path;
+              
+              let parts = path1.split('(');
+              parts = parts[1].split(')');
+              path1 = parseInt(parts[0]);
+              
+              parts = path2.split('(');
+              parts = parts[1].split(')');
+              path2 = parseInt(parts[0]);
+        
+              console.log(path1);
+              console.log(path2);
+              
+              if (path1 > path2) {
+                const temp = paths[i];
+                paths[i] = paths[j];
+                paths[j] = temp;
+              }
+            }
+          }
+        }
+        // const newImage = document.createElement('img');
+
+        // newImage.src = url;
+        // newImage.alt = 'cannot display image';
+
+        // newImage.classList.add('image');
+
+        // newImage.addEventListener('click', () => {
+        //   const slideshowWrapper = document.getElementById('slideshow');
+        //   const imageWrapper = document.getElementById('slideshowImages');
+        //   slideshowWrapper.classList.remove('hide');
+
+        //   currentImage = i + 1;
+          
+        //   document.getElementById('currentImage').src = url;
+
+        //   initButtonWrapper();
+          
+        //   setTimeout(() => {
+        //     slideshowWrapper.style.opacity = 1;
+        //     imageWrapper.style.transform = 'scale(1)';
+        //   }, 10);
+        // });
+        
+        // newRow.appendChild(newImage);
+        
+        // rowCounter++;
+        
+        // if ((rowCounter) % 3 === 0) {
+        //   contentSlideshowWrapper.appendChild(newRow);
+        //   newRow = document.createElement('div');
+        //   rowCounter = 0;
+        // }
+        console.log(paths.length);
+      });
+
+      // i++; 
+    }
+  });
+
+  console.log(paths);
+
+  return paths;
 }
