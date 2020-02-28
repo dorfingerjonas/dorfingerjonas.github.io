95cdefa930aeaebf38e64f051e98bbb9ad73a047
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-02-28 21:55:23 +0100

    add showMoreWorkButton() function

--- script/main.js
+++ script/main.js
@@ -1,3 +1,28 @@
 window.addEventListener('load', () => {
+    const showMoreWorkButton = document.getElementById('showMoreWorkButton');
+    showMoreWorkButton.showMore = true;
+
     AOS.init();
+
+    showMoreWorkButton.addEventListener('click', () => {
+        if (showMoreWorkButton.showMore) {
+            const entries = document.getElementsByClassName('work');
+
+            for (const entry of entries) {
+                entry.classList.remove('hide');
+            }
+
+            showMoreWorkButton.textContent = 'show less';
+        } else {
+            const entries = document.getElementsByClassName('work');
+            
+            for (let i = 3; i < entries.length; i++) {
+                entries[i].classList.add('hide');
+            }
+
+            showMoreWorkButton.textContent = 'show more';
+        }
+
+        showMoreWorkButton.showMore = !showMoreWorkButton.showMore;
+    });
 });
