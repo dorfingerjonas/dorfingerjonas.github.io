84e6c3c1b884b48b40b1e7f49aec704bd1a771f9
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-04 21:27:42 +0100

    remove slideshow

--- script/gallery.js
+++ script/gallery.js
@@ -2,65 +2,65 @@ const howManyImages = 21;
 
 window.addEventListener('load', () => {
     const imageWrapper = document.querySelector('#images');
-    const slideshowImages = document.querySelector('#slideshowImages');
-    const slideshowWrapper = document.getElementById('slideshowWrapper');
-    const disableSlideshow = document.getElementById('disableSlideshow');
-    const previousImage = document.getElementById('previousImageBtn');
-    const nextImage = document.getElementById('nextImageBtn');
-    const close = document.getElementById('close');
-    const width = 70;
+    // const slideshowImages = document.querySelector('#slideshowImages');
+    // const slideshowWrapper = document.getElementById('slideshowWrapper');
+    // const disableSlideshow = document.getElementById('disableSlideshow');
+    // const previousImage = document.getElementById('previousImageBtn');
+    // const nextImage = document.getElementById('nextImageBtn');
+    // const close = document.getElementById('close');
+    // const width = 70;
     let newRow = document.createElement('div');
     let rowCounter = 0;
 
-    window.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowRight' && parseInt(slideshowImages.style.left) - width >= (howManyImages - 1) * (width * (-1)))  {
-            nextImage.click();
-        } else if (event.key === 'ArrowLeft' && parseInt(slideshowImages.style.left) + width <= 0) {
-            previousImage.click();
-        }
-    });
+    // window.addEventListener('keydown', (event) => {
+    //     if (event.key === 'ArrowRight' && parseInt(slideshowImages.style.left) - width >= (howManyImages - 1) * (width * (-1)))  {
+    //         nextImage.click();
+    //     } else if (event.key === 'ArrowLeft' && parseInt(slideshowImages.style.left) + width <= 0) {
+    //         previousImage.click();
+    //     }
+    // });
 
-    nextImage.addEventListener('click', () => {
-        if (parseInt(slideshowImages.style.left) - width >= (howManyImages - 1) * (width * (-1))) {
-            slideshowImages.style.left = `${parseInt(slideshowImages.style.left) - width}vw`;
-        }
-    });
+    // nextImage.addEventListener('click', () => {
+    //     if (parseInt(slideshowImages.style.left) - width >= (howManyImages - 1) * (width * (-1))) {
+    //         slideshowImages.style.left = `${parseInt(slideshowImages.style.left) - width}vw`;
+    //     }
+    // });
 
-    previousImage.addEventListener('click', () => {
-        if (parseInt(slideshowImages.style.left) + width <= 0) {
-            slideshowImages.style.left = `${parseInt(slideshowImages.style.left) + width}vw`;
-        }
-    });
+    // previousImage.addEventListener('click', () => {
+    //     if (parseInt(slideshowImages.style.left) + width <= 0) {
+    //         slideshowImages.style.left = `${parseInt(slideshowImages.style.left) + width}vw`;
+    //     }
+    // });
 
-    close.addEventListener('click', () => {
-        disableSlideshow.click();
-    });
+    // close.addEventListener('click', () => {
+    //     disableSlideshow.click();
+    // });
 
-    disableSlideshow.addEventListener('click', () => {
-        slideshowWrapper.style.opacity = 0; 
-        slideshowWrapper.style.transform = 'scale(1.3)'; 
+    // disableSlideshow.addEventListener('click', () => {
+    //     slideshowWrapper.style.opacity = 0; 
+    //     slideshowWrapper.style.transform = 'scale(1.3)'; 
 
-        setTimeout(() => {
-            slideshowImages.style.transition = 'none';
-            slideshowWrapper.classList.add('hide');
-            disableSlideshow.classList.add('hide');
-            document.body.classList.remove('overflowHidden');
-        }, 260);
-    });
+    //     setTimeout(() => {
+    //         slideshowImages.style.transition = 'none';
+    //         slideshowWrapper.classList.add('hide');
+    //         disableSlideshow.classList.add('hide');
+    //         document.body.classList.remove('overflowHidden');
+    //     }, 260);
+    // });
 
     for (let i = 0; i < howManyImages; i++) {
         const newImage = document.createElement('img');
-        const newSlideshowImage = document.createElement('img');
+        // const newSlideshowImage = document.createElement('img');
 
         newImage.src = `../img/gallery/gallery (${i+1}).jpg`;
         newImage.alt = 'cannot display image';
 
-        newSlideshowImage.src = `../img/gallery/gallery (${i+1}).jpg`;
-        newSlideshowImage.alt = 'cannot display image';
+        // newSlideshowImage.src = `../img/gallery/gallery (${i+1}).jpg`;
+        // newSlideshowImage.alt = 'cannot display image';
 
-        if (i === 12) newSlideshowImage.classList.add('panorama');
+        // if (i === 12) newSlideshowImage.classList.add('panorama');
 
-        slideshowImages.appendChild(newSlideshowImage);
+        // slideshowImages.appendChild(newSlideshowImage);
 
         if (i !== 12) {
             newImage.classList.add('image');  
@@ -68,21 +68,21 @@ window.addEventListener('load', () => {
             newImage.classList.add('panorama');
         }
 
-        newImage.addEventListener('click', () => {
-            slideshowWrapper.classList.remove('hide');
-            disableSlideshow.classList.remove('hide');
-            document.body.classList.add('overflowHidden');
+        // newImage.addEventListener('click', () => {
+        //     slideshowWrapper.classList.remove('hide');
+        //     disableSlideshow.classList.remove('hide');
+        //     document.body.classList.add('overflowHidden');
 
-            setTimeout(() => {
-                slideshowImages.style.left = `${i * -70}vw`;
-                slideshowWrapper.style.opacity = 1; 
-                slideshowWrapper.style.transform = 'scale(1)';
-            }, 10);
+        //     setTimeout(() => {
+        //         slideshowImages.style.left = `${i * -70}vw`;
+        //         slideshowWrapper.style.opacity = 1; 
+        //         slideshowWrapper.style.transform = 'scale(1)';
+        //     }, 10);
 
-            setTimeout(() => {
-                slideshowImages.style.transition = 'left 500ms ease-in-out';
-            }, 200);
-        });
+        //     setTimeout(() => {
+        //         slideshowImages.style.transition = 'left 500ms ease-in-out';
+        //     }, 200);
+        // });
         
         newRow.appendChild(newImage);
 
