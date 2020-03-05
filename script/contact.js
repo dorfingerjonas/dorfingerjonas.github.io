1e1723ffd86f75cb76de0c7e3bf46a4955d75416
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-05 20:15:56 +0100

    add reCAPTCHA and firebase

--- script/contact.js
+++ script/contact.js
@@ -1,3 +1,5 @@
+let reCaptchaCorrect = false;
+
 window.addEventListener('load', () => {
     const email = document.getElementById('email');
     const subject = document.getElementById('subject');
@@ -5,11 +7,23 @@ window.addEventListener('load', () => {
     const errorEmail = document.getElementById('errorEmail');
     const sendEmailBtn = document.getElementById('sendEmail');
 
+    const firebaseConfig = {
+        apiKey: "AIzaSyBd4ZNQRImSA-DLkRuQUShh8jqH-L9DVJM",
+        authDomain: "jonas-dorfinger.firebaseapp.com",
+        databaseURL: "https://jonas-dorfinger.firebaseio.com",
+        projectId: "jonas-dorfinger",
+        storageBucket: "jonas-dorfinger.appspot.com",
+        messagingSenderId: "47344971189",
+        appId: "1:47344971189:web:7dd58623493ae508"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
     sendEmailBtn.addEventListener('click', () => {
         let isValid = true;
 
         // check email
-        if (email.value.replace(' ', '').length === 0) {
+        if (email.value.trim().length === 0) {
             email.classList.add('errorInput');
             errorEmail.textContent = 'email must not be empty';
             isValid = false;
@@ -24,26 +38,54 @@ window.addEventListener('load', () => {
         }
 
         // check subject
-        if (subject.value.replace(' ', '').length === 0) {
-            email.classList.add('errorInput');
+        if (subject.value.trim().length === 0) {
+            subject.classList.add('errorInput');
             errorEmail.textContent = 'subject must not be empty.';
             isValid = false;
         } else {
-            email.classList.remove('errorInput');
+            subject.classList.remove('errorInput');
         }
 
         // check message
-        if (email.value.replace(' ', '').length === 0) {
-            email.classList.add('errorInput');
+        if (message.value.trim().length === 0) {
+            message.classList.add('errorInput');
             errorEmail.textContent = 'message must not be empty.';
             isValid = false;
         } else {
-            email.classList.remove('errorInput');
+            message.classList.remove('errorInput');
+        }
+
+        if (!reCaptchaCorrect) {
+            errorEmail.textContent = 'you have to complete reCAPTCHA.';
+            isValid = false;
         }
         
+        if (isValid) {
+            firebase.database().ref('public/email/' + new Date().getTime()).set({
+                message: message.value,
+                email: email.value,
+                subject: subject.value
+            }).then(() => {
+                email.value = '';
+                subject.value = '';
+                message.value = '';
+
+                errorEmail.style.color = 'black';
+                errorEmail.textContent = 'message sent successfully!';
+            
+                setTimeout(() => {
+                    errorEmail.innerHTML = '&nbsp;';
+                    errorEmail.style.color = 'red';
+                }, 3500);
+            });
+        }
     });
 });
 
 function validateEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function recaptchaCompleted() {
+    reCaptchaCorrect = true;
 }
