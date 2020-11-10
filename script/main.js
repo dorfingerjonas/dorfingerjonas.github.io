e09eca375f9ae83d0c8800b95085e2aca484c33b
dorfingerjonas <jonas.dorfinger@gmx.at>
2020-11-10 23:32:00 +0100

    remove show more images button

--- script/main.js
+++ script/main.js
@@ -1,11 +1,9 @@
 window.addEventListener('load', () => {
-    const showMoreImagesButton = document.getElementById('showMoreImagesButton');
     const workWrapper = document.getElementById('workWrapper');
     const currentWorkPart = document.getElementsByClassName('currentWorkPart');
     let width = 50;
     let interval;
     let isFirstPageActive = true;
-    showMoreImagesButton.showMore = true;
 
     AOS.init();
     
@@ -39,27 +37,6 @@ window.addEventListener('load', () => {
         currentWorkPart[0].style.background = 'white';
         currentWorkPart[1].style.background = 'lightgray';
         createInterval();
-    });
-
-    
-    showMoreImagesButton.addEventListener('click', () => {
-        const entries = document.getElementById('images').childNodes;
-        
-        if (showMoreImagesButton.showMore) {
-            for (const entry of entries) {
-                entry.classList.remove('hide');
-            }
-
-            showMoreImagesButton.textContent = 'show less';
-        } else {            
-            for (let i = 3; i < entries.length; i++) {
-                entries[i].classList.add('hide');
-            }
-
-            showMoreImagesButton.textContent = 'show more';
-        }
-
-        showMoreImagesButton.showMore = !showMoreImagesButton.showMore;
     });
     
     function createInterval() {
