d96cd0bcf4333995fbc041c40b6dd24772c7d631
dorfingerjonas <jonas.dorfinger@gmx.at>
2020-11-14 21:09:02 +0100

    implement firebase storage file server for gallery images

--- script/gallery.js
+++ script/gallery.js
@@ -1,20 +1,47 @@
 window.addEventListener('load', () => {
+    const storage = firebase.storage();
+    const listRef = storage.ref().child('gallery/preview');
     const imageWrapper = document.getElementById('images');
 
-    for (let i = 0; i < 4; i++) {
-        const column = document.createElement('div');
+    listRef.listAll().then(res => {
+        createColumns(4);
+        createImages(res.items.length);
 
-        column.id = `column${i + 1}`;
-        column.classList.add('column');
+        res.items.forEach(itemRef => {
+            storage.ref(itemRef.fullPath).getDownloadURL().then(url => {
+                document.getElementById(`image${getIdOfPath(itemRef.fullPath)}`).src = url;
+            }).catch(err => {
+                console.error(err.message);
+            }); 
+        });
+    }).catch(err => {
+        console.error(err.message);
+    });
 
-        imageWrapper.appendChild(column);
+    function createColumns(amount) {
+        for (let i = 0; i < amount; i++) {
+            const column = document.createElement('div');
+    
+            column.id = `column${i + 1}`;
+            column.classList.add('column');
+    
+            imageWrapper.appendChild(column);
+        }
     }
 
-    for (let i = 1; i <= 24; i++) {
-        const newImage = document.createElement('img');
-        newImage.src = `../img/gallery/gallery_(${i}).jpg`;
-        newImage.alt = 'cannot display image';
-        newImage.classList.add('image');
-        document.getElementById(`column${(i % 4) + 1}`).appendChild(newImage);
+    function createImages(amount) {
+        for (let i = 0; i < amount; i++) {
+            const newImage = document.createElement('img');
+
+            newImage.alt = 'cannot display image';
+            newImage.id = `image${i + 1}`;
+            newImage.classList.add('image');
+
+            document.getElementById(`column${((i + 1) % 4) + 1}`).appendChild(newImage);
+        }
+    }
+
+    function getIdOfPath(path) {
+        return parseInt(path.substring(25, path.length - 5));
     }
 });
