d0790a78e1418dbb238a34ba0099e18e2c0e4f86
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-13 17:41:15 +0100

    #24 create fields

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  title = 'About';
+  portraitPath = '/assets/img/portrait.jpg';
+  items = [
+    'I am a web designer and photographer with three years of coding & design experience. My knowledge in project management, design, coding, and strengths in social communication shape me and my personality.',
+    'To get new ideas and support my creativity, I spend a lot of time in the mountains skiing. Sometimes, my best ideas grow while running in the morning around my hometown. The most effective time to work for me is in the morning. Although it is early, I am focused on completing tasks to achieve my daily goals.',
+    'I take my motivation from satisfied customers or colleagues with which I worked. A motivating impulse is to see the completed product, especially the moment you get a smile or just a simple "Thank you" from other people.',
+    'I believe learning by doing is the most effective way to get into new stuff. This mentality helped me to enhance my skill and always keep improving. I am currently a student who is interested in new challenges. Do not hesitate, contact me using the contact form on this page.',
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
